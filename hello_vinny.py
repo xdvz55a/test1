fa4a38d642c0424e99241e70038970326ee3d5c2
Xdvz55a <richardleto@yahoo.com>
2018-09-10 16:47:32 -0400

    Update hello_vinny.py

--- hello_vinny.py
+++ hello_vinny.py
@@ -1,1 +1,1 @@
-print "Hello Vinny, What's new?  How is your day today?  Are you playing air hockey?"
+print "Hello Vinny, What's new?  How is your day today?  Are you playing air hockey with Matthew?"
